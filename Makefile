ea333fdd857a0c83c34341b9845f4e9c52fe2ed8
Aziz Fall <azizfall@MacBook-Air-de-Aziz.local>
2021-04-26 11:44:05 +0200

    correct error duplication and add file const.c to solve the error

--- Makefile
+++ Makefile
@@ -1,17 +1,20 @@
 run: main 
 	./main 
 
-main: encryption.o main.o decryption.o
-	gcc	*.o -o main 
+main: encryption.o main.o decryption.o const.o
+	gcc	*.o -o main -fcommon
 
 main.o: main.c encryption.h decryption.h
-	gcc -c main.c
+	gcc -c main.c -fcommon
 
 encryption.o: encryption.h encryption.c const.h
-	gcc -c encryption.c
+	gcc -c encryption.c -fcommon
 
 decryption.o: decryption.h decryption.c const.h
-	gcc -c decryption.c
+	gcc -c decryption.c -fcommon
+
+const.o: const.c const.h 
+	gcc -c const.c -fcommon
 
 clean:
 	rm -fr *.o main 
--- const.h
+++ const.h
@@ -4,7 +4,7 @@
 #include <inttypes.h>
 #include <stdint.h>
 
-const __uint128_t BITS_PIVOTED = 0x1fffffffffffffff;
+extern const __uint128_t BITS_PIVOTED; 
 
 /**
  * @brief WORDS is an array that contains 
@@ -12,40 +12,26 @@ const __uint128_t BITS_PIVOTED = 0x1fffffffffffffff;
  * are between positions 0 and 3, 4 and 7,
  * ..., 20 and 23.
  */
-const uint32_t WORDS[6] = { 
-                            0xf,    0xf0,    0xf00, 
-                            0xf000, 0xf0000, 0xf00000
-                        };
+extern const uint32_t WORDS[6];
 
 /**
- * @brief SUBS an array of substitution, 
+ * @brief SUBS is an array of substitution, 
  * each cage of table contains 
  * corresponding bit position.
  */
-const uint16_t SUBS[16] = {
-                            0xc, 0x5, 0x6, 0xb,
-                            0x9, 0x0, 0xa, 0xd,
-                            0x3, 0xe, 0xf, 0x8,
-                            0x4, 0x7, 0x1, 0x2
-                        };
+extern const uint16_t SUBS[16];
 
-const uint8_t PERM[24] = {
-                            0x0,  0x6,  0xc, 0x12, 0x1,  0x7,
-                            0xd,  0x13, 0x2, 0x8,  0xe,  0x14,
-                            0x3,  0x9,  0xf, 0x15, 0x4,  0xa,
-                            0x10, 0x16, 0x5, 0xb,  0x11, 0x17
-                        };
+/**
+ * @brief PERM is an array of permutation 
+ *, each cage of table contains the position
+ * of the bit to permute.
+ */
+extern const uint8_t PERM[24];
 
 /**
  * @brief POS is an array of bits position
  * between 0 to 24.
  */
-const uint32_t POS[24] = {  0x1,      0x2,      0x4,      0x8, 
-                            0x10,     0x20,     0x40,     0x80,
-                            0x100,    0x200,    0x400,    0x800,
-                            0x1000,   0x2000,   0x4000,   0x8000,
-                            0x10000,  0x20000,  0x40000,  0x80000,
-                            0x100000, 0x200000, 0x400000, 0x800000
-                        };
+extern const uint32_t POS[24];
 
 #endif

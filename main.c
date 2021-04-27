cceb86c2373031202e07aa7eaba7278dd6529d96
Aziz Fall <azizfall@MacBook-Air-de-Aziz.local>
2021-04-27 13:06:56 +0200

    function rotated bits is done successfuly

--- main.c
+++ main.c
@@ -5,8 +5,25 @@ int main(int argc, char const *argv[])
 {
     uint80_t *key = init();
     init_key(0xf34ab7, key);
+    printf(" _________ Before rotation _________\n");
     print_key(*key);
     rotate_bits(key);
+    printf(" _________ 1 in rotation _________\n");
+    print_key(*key);
+    rotate_bits(key);
+    printf(" _________ 2 in rotation _________\n");
+    print_key(*key);
+    rotate_bits(key);
+    printf(" _________ 3 in rotation _________\n");
+    print_key(*key);
+    rotate_bits(key);
+    printf(" _________ 4 in rotation _________\n");
+    print_key(*key);
+    rotate_bits(key);
+    printf(" _________ 5 in rotation _________\n");
+    print_key(*key);
+    rotate_bits(key);
+    printf(" _________ 6 in rotation _________\n");
     print_key(*key);
 
     uint32_t a = boxS(2); 
